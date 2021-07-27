26514e556f24bcca2e10600589ebb198a0e2d44a
Audrius <86117093+Aruidas@users.noreply.github.com>
2021-07-27 16:09:00 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,35 +1,55 @@
-const hamburger = document.getElementById("hamburger");
-const navMenu = document.getElementById("nav-menu");
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
 
 function toggleMenu() {
-    hamburger.classList.toggle("hamburger-active");
-    navMenu.classList.toggle("open");
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('open');
 }
 
 
-const sections = document.querySelectorAll(".anchor");
-const listItems = document.querySelectorAll("#nav-menu li");
+const sections = document.querySelectorAll('.anchor');
+const listItems = document.querySelectorAll('#nav-menu li');
 
-window.addEventListener("scroll", ()=> {
-    let currentSection = "";
+window.addEventListener('scroll', () => {
+    let currentSection = '';
 
     sections.forEach(section => {
         const sectionTop = section.offsetTop;
-        const sectionHeight = section.clientHeight;
-        if(pageYOffset >= sectionTop - 50) { // -X: offset, to highlight links earlier
-            currentSection = section.getAttribute("id");
+        if(pageYOffset >= sectionTop - 175) { // -X: offset, to highlight links earlier
+            currentSection = section.getAttribute('id');
         }
     })
 
     listItems.forEach(li => {
-        li.classList.remove("current");
-        if(li.firstElementChild.getAttribute("href") == "#"+currentSection) {
-            li.classList.add("current");
+        li.classList.remove('current');
+        if(li.firstElementChild.getAttribute('href') == '#'+currentSection) {
+            li.classList.add('current');
         }
     })
 })
 
+
 function closeMobileMenu() {
-    hamburger.classList.remove("hamburger-active");
-    navMenu.classList.remove("open");
+    hamburger.classList.remove('hamburger-active');
+    navMenu.classList.remove('open');
+}
+
+
+function expandText() {
+    this.style.display = 'none';
+    this.nextElementSibling.style.display = 'block';
+}
+
+function toggleTheme() {
+    if(this.firstElementChild.classList.contains('fa-moon')) {
+        this.firstElementChild.classList.remove('fa-moon');
+        this.firstElementChild.classList.add('fa-sun');
+        document.documentElement.style.setProperty('--colorLight', '#00072b');
+        document.documentElement.style.setProperty('--colorDark', '#ffd800');
+    } else {
+        this.firstElementChild.classList.remove('fa-sun');
+        this.firstElementChild.classList.add('fa-moon');
+        document.documentElement.style.setProperty('--colorLight', '#fff');
+        document.documentElement.style.setProperty('--colorDark', '#131313');
+    }
 }
